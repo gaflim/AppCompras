e1d86c6392ac5cb7a24cb7c0691383ab3cc11e73
Galim <gablima41@gmail.com>
2025-02-27 17:43:02 -0300

    initial commit

---
+++ AppCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,45 @@
+using AppCompras.Models;
+using SQLite;
+
+namespace AppCompras.Helpers;
+
+public class SQLiteDatabaseHelper
+{
+    readonly SQLiteAsyncConnection _con;
+
+    public SQLiteDatabaseHelper(string path)
+    {
+        _con = new SQLiteAsyncConnection(path);
+        _con.CreateTableAsync<Produto>().Wait();
+    }
+
+    public Task<int> Insert(Produto p)
+    {
+        return _con.InsertAsync(p);
+    }
+
+    public Task<List<Produto>> Update(Produto p)
+    {
+        string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+        
+        return _con.QueryAsync<Produto>(
+            sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+        );
+    }
+
+    public Task<int> Delete(int id)
+    {
+        return _con.Table<Produto>().DeleteAsync(i => i.Id == id);
+    }
+
+    public Task<List<Produto>> GetAll()
+    {
+        return _con.Table<Produto>().ToListAsync();
+    }
+
+    public Task<List<Produto>> Search(string query)
+    {
+        string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%'+'query'+'%'";
+        return _con.QueryAsync<Produto>(sql);
+    }
+}
---
+++ AppCompras/Models/Produto.cs
@@ -0,0 +1,14 @@
+using Android.AdServices.AppSetIds;
+
+namespace AppCompras.Models;
+
+using SQLite;
+
+public class Produto
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public double Quantidade { get; set; }
+    public double Preco { get; set; }
+}
